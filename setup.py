eafd690d2211a3e2519e23e771201c2609e0edf8
pieby2 <prakharpragyan1000@gmail.com>
2025-12-17 13:20:09 +0530

    Rename to autoimpute-pieby2 to ensure uniqueness

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ else:
     ]
 
 setuptools.setup(
-    name="autoimpute-lib",
+    name="autoimpute-pieby2",
     version="1.0.81",
     description="AutoImpute - Missing Data Imputation Framework for Machine Learning",
     long_description=long_description,      
